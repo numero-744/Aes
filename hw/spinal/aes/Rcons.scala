145441b4b49e010ce47863e6e4d82891c24e3443
Côme ALLART <numero_744@net-c.com>
2022-11-28 21:07:09 +0100

    add -> alignment

--- hw/spinal/aes/Rcons.scala
+++ hw/spinal/aes/Rcons.scala
@@ -3,16 +3,16 @@ package aes
 import spinal.core._
 
 object Rcons {
-  def apply(i: UInt): UInt = Vec(
-    U"8'h1",
-    U"8'h2",
-    U"8'h4",
-    U"8'h8",
-    U"8'h10",
-    U"8'h20",
-    U"8'h40",
-    U"8'h80",
-    U"8'h1b",
-    U"8'h36"
-  )(i)
+  def apply(i: UInt): UInt = i.mux(
+    default -> U"8'h1",
+    0x1     -> U"8'h2",
+    0x2     -> U"8'h4",
+    0x3     -> U"8'h8",
+    0x4     -> U"8'h10",
+    0x5     -> U"8'h20",
+    0x6     -> U"8'h40",
+    0x7     -> U"8'h80",
+    0x8     -> U"8'h1b",
+    0x9     -> U"8'h36"
+  )
 }
